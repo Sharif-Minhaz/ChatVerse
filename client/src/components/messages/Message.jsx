e711b23de43be9514321c36e72acf2a42b83f161
Minhaz <sharif35-3001@diu.edu.bd>
2024-02-09 22:22:27 +0600

    feat: search added - client

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,33 +1,30 @@
-// import { useAuthContext } from "../../context/AuthContext";
-// import { extractTime } from "../../utils/extractTime";
-// import useConversation from "../../zustand/useConversation";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { extractTime } from "../../utils";
+import useConversation from "../../store/useConversation";
 
-const Message = () => {
-	// const { authUser } = useAuthContext();
-	// const { selectedConversation } = useConversation();
-	// const fromMe = message.senderId === authUser._id;
-	// const formattedTime = extractTime(message.createdAt);
-	// const chatClassName = fromMe ? "chat-end" : "chat-start";
-	// const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-	// const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-	// const shakeClass = message.shouldShake ? "shake" : "";
+	const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
-		<div className={`chat ${"chatClassName"}`}>
+		<div className={`chat ${chatClassName}`}>
 			<div className="chat-image avatar">
 				<div className="w-10 rounded-full">
-					<img
-						alt="Tailwind CSS chat bubble component"
-						src={"https://mui.com/static/images/avatar/2.jpg"}
-					/>
+					<img alt="Tailwind CSS chat bubble component" src={profilePic} />
 				</div>
 			</div>
-			<div className={`chat-bubble text-white ${"bubbleBgColor"} ${"shakeClass"} pb-2`}>
-				{"message.message"}
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>
+				{message.message}
 			</div>
 			<div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-				{"formattedTime"}
+				{formattedTime}
 			</div>
 		</div>
 	);
